29f3d4eeb9ff5a50be8e86ee70f560a21e6d67bb
rotexdev@gmail.com <rotexdev@gmail.com@038c33e6-2d7b-11de-8ffe-b512714d229c>
2013-03-28 18:17:56 +0000

    Updated package names to the new domain vldocking.com, updated build.xml updated javadoc_vldocking_3.0.0.zip, and vldocking_3.0.0.jar Moved all images into src/resources.

---
+++ trunk/src/src/com/vldocking/swing/toolbars/ToolBarIOReadInfo.java
@@ -0,0 +1,68 @@
+package com.vldocking.swing.toolbars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Descriptor of the loading operation of toolbars.
+ * 
+ * <p> returns useful informations such as the toolbars registered and
+ * successfully loaded, and the ones not loaded.
+ * 
+ * @author Lilian Chamontin, VLSolutions
+ */
+public class ToolBarIOReadInfo {
+
+	List<?> notInstalledToolBarList;
+	List<VLToolBar> installedToolBarList = new ArrayList<VLToolBar>();
+	List<String> unknownToolBarNames = new ArrayList<String>();
+	List<?> registeredToolbars;
+
+	public ToolBarIOReadInfo() {}
+
+	/**
+	 * returns the list of toolbars that were successfully loaded from the
+	 * toolbarIO operation
+	 */
+	public List<VLToolBar> getInstalledToolbars() {
+		return installedToolBarList;
+	}
+
+	/**
+	 * returns the list of toolbars that weren't loaded from the stream (but
+	 * registered)
+	 */
+	public List<?> getNotInstalledToolbars() {
+		return notInstalledToolBarList;
+	}
+
+	/**
+	 * Return the list of toolbars names (String) found in the stream but not
+	 * registered (these toolbars cannot be loaded, so the API can only report
+	 * their name).
+	 */
+	public List<String> getUnknownToolbarNames() {
+		return unknownToolBarNames;
+	}
+
+	void notifyToolbarInstalled(VLToolBar tb) {
+		installedToolBarList.add(tb);
+	}
+
+	void setRegisteredToolbars(List<?> registered) {
+		this.registeredToolbars = registered;
+	}
+
+	/** invoked on completion of loading */
+	void finishLoading() {
+
+		this.notInstalledToolBarList = new ArrayList<Object>(registeredToolbars);
+		notInstalledToolBarList.removeAll(installedToolBarList);
+
+	}
+
+	void notifyUnknownToolbarFound(String toolbarName) {
+		unknownToolBarNames.add(toolbarName);
+	}
+
+}
